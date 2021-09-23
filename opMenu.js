1af8c345b987cf8f7df015fb1d749ce17235f2e4
RafaelCruzJr <46229691+RafaelCruzJr@users.noreply.github.com>
2021-09-23 02:26:57 -0400

    Add files via upload

---
+++ opMenu.js
@@ -0,0 +1,58 @@
+var text = document.getElementById('opInstructions'), // grab instructions element
+	nextButton = document.getElementById('next'), // grab next button element
+	previousButton = document.getElementById('previous'), // grab previous button element
+	prac = document.getElementById('prac'), // grab practice button element
+	op = document.getElementById('op'), // grab operation span button element
+	instructions = [
+	"For this portion of the task, you will be performing a memory task at the same time as solving math equations.",
+	"A letter will appear (ex: F), then a simple math equation (ex: [5/5] + 2 = 3), then another letter, and another equation, and so on for anywhere from 3 to 9 cycles.",
+	"The math equations may be correct or incorrect. When an equation is shown to you, you must respond with either 'A' for correct or 'L' for not.",
+	"Your job is to remember the letters and in which order they appeared, while accurately deciding whether the math equations are correct or not.",
+	"At the end of each set of cycles you will be asked to input in which order the letters appeared. There will be 7 sets of cycles (3 to 9) and you will do this twice for a total of 14 sets.",
+	"Try to be as accurate as possible with the equations, while remembering as many letters as possible.",
+	"You must respond to the equations within a certain time limit based on how well you did during the practice equations.",
+	"Let's first do a short practice set of 5 letters to allow you to get used to the format. Press the button when you are ready.",
+	"You should have entered '1','2','3','4','5' for 'D','K','I','P','H', respectively.",
+	"Let's now move on to the main task. You will be expected to correctly answer at least 75% of the math equations within the time limit for the main task.",
+	"You will be given a break halfway through the task, during which your accuracy will be shown to you. Press the button when ready."], // series of instructions
+	numIns = 0; // initialize instruction number
+
+prac.addEventListener("click",function() {numIns++; changeText(instructions[numIns]);}); // onClick function to continue after practice
+
+function next() {
+	numIns++;
+	if (numIns == 1) {
+		previousButton.style.display = 'block';
+	}
+	if (numIns == 7) {
+		nextButton.style.display = 'none';
+		prac.style.display = 'block';
+	}
+	if (numIns == 9) {
+		previousButton.style.display = 'block';
+	}
+	if (numIns == 10) {
+		nextButton.style.display = 'none';
+		op.style.display = 'block';
+	}
+	text.innerHTML = instructions[numIns]; // display the current instruction text
+}
+
+function previous() {
+	numIns--;
+	if (numIns == 0) {
+		previousButton.style.display = 'none';
+	}
+	if (numIns == 6) {
+		nextButton.style.display = 'block';
+		prac.style.display = 'none';
+	}
+	if (numIns == 8) {
+		previousButton.style.display = 'none';
+	}
+	if (numIns == 9) {
+		nextButton.style.display = 'block';
+		op.style.display = 'none';
+	}
+	text.innerHTML = instructions[numIns]; // display the current instruction text
+}
---
+++ opPractice.js
@@ -0,0 +1,118 @@
+function runPractice() {
+	var allTrials = [['D','6 / 2 + 5 = 9',0],['K','5 * 3 - 2 = 13',1],['I','4 * 2 + 6 = 18',1],['P','8 / 2 - 3 = 1',1],['H','9 * 2 - 3 = 4',0]], // full set of practice trials
+		timePrompt = Number(opener.document.getElementById("arithAVG").value), // grab average from arithmetic
+		SD = Number(opener.document.getElementById("arithSD").value),// grab SD from arithmetic
+		timerID, // initialize eventTimer ID
+		trialN = 0, // initialize trial number
+		labels = ['lab1','lab2','lab3','lab4','lab5','lab6','lab7','lab8','lab9','lab10','lab11','lab12'], // labels associated with response screen
+		inputs = ['let1','let2','let3','let4','let5','let6','let7','let8','let9','let10','let11','let12'], // actual inputs
+		attend = document.getElementById("attend"), // grab element for showing prompts
+		opDiv = document.getElementById("opDiv"), // grab element for response screen
+		opButton = document.getElementById("opButton"); // grab element for span task button
+		pracButton = document.getElementById("pracButton"); // grab element for practice button
+
+	document.getElementById("holder").style.display = 'none'; // remove instructions
+
+	if (document.documentElement.requestFullscreen) {
+		document.documentElement.requestFullscreen();
+	} else if (document.documentElement.webkitRequestFullscreen) {
+		document.documentElement.webkitRequestFullscreen();
+	} else if (document.documentElement.msRequestFullscreen) {
+		document.documentElement.msRequestFullscreen();
+	} // for setting it full screen depending on web browser
+
+	function displayAttend(newText = '*****') {
+		attend.innerHTML = newText;
+		attend.style.display = 'block';
+	} // make displaying attention simple
+
+	function removeAttend() {
+		attend.style.display = 'none';
+	} // make removing attention simple
+
+	function mathKeys(event) {
+		switch (event.key) {
+			case 'a':
+				document.removeEventListener('keydown',mathKeys); // remove eventListener
+				removeAttend();
+				eventTimer.cancelRequest(timerID); // stop "slow" from showing
+				eventTimer.setTimeout(endTrial,200); // end current trial
+				break;
+			case 'l':
+				document.removeEventListener('keydown',mathKeys);
+				removeAttend();
+				eventTimer.cancelRequest(timerID);				
+				eventTimer.setTimeout(endTrial,200);
+				break;
+			default:
+				break;					
+		}
+	}
+
+	// function to run 1 entire trial of operation span
+	function runSeq() {
+		displayAttend(allTrials[trialN][0]); // display letter
+		eventTimer.setTimeout(removeAttend,2000); // remove it
+		eventTimer.setTimeout(function() {displayAttend(allTrials[trialN][1]); document.addEventListener('keydown',mathKeys);},3000); // display equation and listen for response
+		timerID = eventTimer.setTimeout(slow, 3000 + timePrompt + SD); // prepare "slow" function if too slow
+	}
+
+	function slow() { // show "too slow" if no response
+		document.removeEventListener('keydown',mathKeys);
+		displayAttend('Too Slow');
+		eventTimer.setTimeout(endTrial, 200)
+	}
+
+	function endTrial() { // either go to next trial or show answer screen
+		removeAttend();
+		trialN++;
+		if (trialN == 5) {
+			answerScreen();
+		} else {
+			runSeq();
+		}
+	}
+
+	// function to create the screen to allow pariticpants to input in which order they saw letters
+	function answerScreen() {
+		var options = ['D','K','I','P','H'], // shuffle options
+			letCopy = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; // copy of the alphabet
+		for (i = 0; i < options.length; i++) { // iterate through letters
+			letCopy.splice(letCopy.indexOf(options[i]),1); // remove the correct answers from letters
+		}
+		for (i = 0; i < 7; i++) { // iterate through letters for the remaining number of options
+			idxA = Math.floor(Math.random() * letCopy.length); // index randomly into letters
+			alpha = letCopy[idxA]; // grab letter
+			letCopy.splice(idxA,1); // remove it from list of letters
+			options.push(alpha); // put it into options
+		}
+		shuffle(options);
+		for (i = 0; i < 12; i++) { // iterate through options
+			document.getElementById(labels[i]).innerHTML = options[i]; // place them in HTML
+		}
+		opDiv.style.display = 'block'; // display HTML of screen
+	}
+
+	// function for submit button for answer screen
+	pracButton.onclick = function() {
+		for (i = 0; i < 12; i++) { // iterate through HTML elements
+			document.getElementById(inputs[i]).value = ''; // blank them
+		}
+		opDiv.style.display = 'none'; // remove answer screen
+		pracButton.style.display = 'none';
+		opButton.style.display = 'block';
+		document.getElementById("holder").style.display = 'block';
+		document.getElementById("next").style.display = 'block';
+		document.getElementById("previous").style.display = 'none';
+		document.getElementById("prac").style.display = 'none';
+
+	}
+
+	eventTimer.setMultipleTO([ // show a countdown, then start practice
+		[function() {displayAttend('3');}, 100], 
+		[function() {displayAttend('2');}, 1100],
+		[function() {displayAttend('1');}, 2100],
+		[removeAttend, 3100],
+		[runSeq, 3400]
+	])	
+}
